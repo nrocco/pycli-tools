bd36861e7a0b09901a5232c0dacb480e0358e674
Nico Di Rocco <dirocco.nico@gmail.com>
2013-05-10 16:59:06 +0200

    Extending ArgumentParser class does not work for subparsers

--- pycli/__init__.py
+++ pycli/__init__.py
@@ -9,91 +9,86 @@ from argparse import ArgumentParser
 log = logging.getLogger(__name__)
 
 
-class ArgumentSuperParser(ArgumentParser):
-
-    opt_config  = ('-c', '--config',  'path to the config file')
-    opt_verbose = ('-v', '--verbose', 'output more verbose')
-    opt_quiet   = ('-q', '--quiet',   'surpress all output')
-    opt_version = ('-V', '--version', '%(prog)s ')
-
-    def __init__(self, prog, version, default_config=None, *args, **kwargs):
-        self._prog = prog
-        self._version = version
-        self._default_config_file = default_config
-
-        self._main = self.get_main_parser(self._prog)
-        self._main_argv, self._remaining_argv = self._main.parse_known_args()
-
-        if self._main_argv.quiet:
-            self._loglevel = 100
-        else:
-            self._loglevel = max(30 - self._main_argv.verbose * 10, 10)
-
-        logging.basicConfig(level=self._loglevel, format='%(message)s')
-
-        self._config_file = path.expanduser(self._main_argv.config_file)
-
-        if self._config_file:
-            if path.isfile(self._config_file):
-                log.info('Reading default configuration from %s', self._config_file)
-                from ConfigParser import ConfigParser
-                self._config = ConfigParser()
-                self._config.read(self._config_file)
-                self._default_config = self.parse_config_defaults(self._config, self._prog)
-            else:
-                log.debug('Ignoring non-existent config file: %s', self._config_file)
-                self._config = None
-                self._default_config = {}
-        else:
-            self._config = None
-            self._default_config = {}
-
-
-        super(ArgumentSuperParser, self).__init__(prog=self._prog, parents=[self._main], *args, **kwargs)
-        self.add_opt_version(self._version, self)
-        self.set_defaults(verbose=self._main_argv.verbose,
-                            quiet=self._main_argv.quiet, **self._default_config)
-
-
-
-
-    def get_main_parser(self, prog):
-        mp = ArgumentParser(prog=prog, add_help=False)
-        self.add_opt_config(mp)
-        self.add_opt_verbose(mp)
-        self.add_opt_quiet(mp)
-        return mp
-
-
-    def parse_config_defaults(self, parser, section):
-        if parser.has_section(section):
-            return dict(parser.items(section))
-        else:
-            log.debug('Section %s not found. Ignoring.', section)
-            return {}
-
-
-    def add_opt_config(self, parser):
-        o = self.opt_config
-        parser.add_argument(o[0], o[1], help=o[2], dest='config_file',
-                            default=self._default_config_file)
-
-
-    def add_opt_verbose(self, parser):
-        o = self.opt_verbose
-        parser.add_argument(o[0], o[1], default=0, action='count', help=o[2])
-
-
-    def add_opt_quiet(self, parser):
-        o = self.opt_quiet
-        parser.add_argument(o[0], o[1], action='store_true', help=o[2])
-
-
-    def add_opt_version(self, version, parser):
-        v = self.opt_version
-        parser.add_argument(v[0], v[1], action='version', version=v[2] + version)
-
-
+class SuperArgParser(ArgumentParser):
+    remaining_args = None
     def parse_args(self, *args, **kwargs):
-        self._args = super(ArgumentSuperParser, self).parse_args(self._remaining_argv)
-        return self._args
+        if self.remaining_args:
+            return super(SuperArgParser, self).parse_args(self.remaining_args)
+        else:
+            return super(SuperArgParser, self).parse_args(*args, **kwargs)
+
+
+def get_main_parser(prog, default_config_file):
+    parser = ArgumentParser(prog=prog, add_help=False)
+    parser.add_argument('-c', '--config', dest='config_file',
+                        help='path to the config file',
+                        default=default_config_file)
+    parser.add_argument('-v', '--verbose', default=0, action='count',
+                        help='output more verbose')
+    parser.add_argument('-q', '--quiet', action='store_true',
+                        help='surpress all output')
+    return parser
+
+
+
+def parse_config_defaults(parser, section):
+    if parser.has_section(section):
+        return dict(parser.items(section))
+    else:
+        log.debug('Section %s not found. Ignoring.', section)
+        return {}
+
+
+
+def get_argparser(*args, **kwargs):
+    prog = kwargs.get('prog')
+    version = kwargs.pop('version', '')
+    default_config_file = kwargs.pop('default_config', None)
+
+    mainparser = get_main_parser(prog, default_config_file)
+    main_args, remaining_args = mainparser.parse_known_args()
+
+    if main_args.quiet:
+        loglevel = 100
+    else:
+        loglevel = max(30 - main_args.verbose * 10, 10)
+
+    logging.basicConfig(level=loglevel, format='%(message)s')
+
+    if main_args.config_file:
+        config_file = path.expanduser(main_args.config_file)
+    else:
+        config_file = None
+
+    if config_file:
+        if path.isfile(config_file):
+            log.info('Reading default configuration from %s', config_file)
+            from ConfigParser import ConfigParser
+            config = ConfigParser()
+            config.read(config_file)
+            default_config = parse_config_defaults(config, prog)
+        else:
+            log.debug('Ignoring non-existent config file: %s', config_file)
+            config = None
+            default_config = {}
+    else:
+        config = None
+        default_config = {}
+
+
+    parser = SuperArgParser(prog=prog, parents=[mainparser])
+    parser.remaining_args = remaining_args
+    parser.add_argument('-V', '--version', action='version',
+                        version='%(prog)s ' + version)
+    parser.set_defaults(
+        prog = prog,
+        version = version,
+        default_config_file = default_config_file,
+        config_file = config_file,
+        loglevel = loglevel,
+        verbose = main_args.verbose,
+        quiet = main_args.quiet,
+        **default_config
+    )
+
+    return parser
