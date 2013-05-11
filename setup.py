e3d1ae816cb32a86c64cccb293188407461e1a4b
Nico Di Rocco <dirocco.nico@gmail.com>
2013-05-11 23:54:43 +0200

    Updated setup.py for pypi distribution

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 from setuptools import setup
+import pycli_tools
 
 setup(
     name = 'pycli_tools',
-    version = '1.1',
+    version = pycli_tools.__version__,
     packages = [
         'pycli_tools'
     ],
+    download_url = 'http://github.com/nrocco/pycli-tools/archive/master.zip',
     url = 'http://nrocco.github.io/',
-    author = 'Nico Di Rocco',
+    author = pycli_tools.__author__,
     author_email = 'dirocco.nico@gmail.com',
     description = 'A python module to help create predictable command line tools',
+    long_description = pycli_tools.__doc__,
     include_package_data = True,
     classifiers = [
         'Operating System :: OS Independent',
