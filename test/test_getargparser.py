928e232e16becb9c2cfd11cfddbdb5e317b77cd8
Nico Di Rocco <dirocco.nico@gmail.com>
2013-05-13 21:29:37 +0200

    Added more tests to reach 98% code coverage

--- test/test_getargparser.py
+++ test/test_getargparser.py
@@ -9,6 +9,16 @@ from pycli_tools import get_argparser
 sys.argv = ['pycli_test']
 
 
+def test_processing_remaining_argv():
+    sys.argv.append('test')
+    parser = get_argparser(prog='myapp', version='1.0',
+                           default_config='~/.myapprc')
+    parser.add_argument('event')
+    args = parser.parse_args()
+    assert args.event == 'test'
+    sys.argv.pop()
+
+
 def test_with_default_config():
     parser = get_argparser(prog='myapp', version='1.0',
                            default_config='~/.myapprc')
@@ -23,18 +33,51 @@ def test_with_default_config():
 
 
 def test_overriding_default_config():
-    arguments = '-c test/tests.conf'.split()
+    arguments = '-c test/myapp.conf'.split()
     parser = get_argparser(prog='myapp', version='1.7',
                            default_config='~/.myapprc',
                            arguments=arguments)
     args = parser.parse_args()
-    assert args.config_file == 'test/tests.conf'
+    assert args.config_file == 'test/myapp.conf'
     assert args.default_config_file == '~/.myapprc'
+    assert args.prog == 'myapp'
+    assert args.quiet == False
+
+
+def test_parsing_config_file():
+    parser = get_argparser(prog='myapp', version='1.7',
+                           default_config='test/myapp.conf')
+    args = parser.parse_args()
+    assert args.config_file == 'test/myapp.conf'
+    assert args.default_config_file == 'test/myapp.conf'
+    assert args.verbose == 0
     assert args.loglevel == 30
     assert args.prog == 'myapp'
     assert args.quiet == False
+    assert args.username == 'earl'
+
+
+def test_ignoring_config_no_section():
+    parser = get_argparser(prog='nonexistentsection', version='1.1.1',
+                           default_config='test/myapp.conf')
+    args = parser.parse_args()
+    assert args.prog == 'nonexistentsection'
+
+
+def test_parsing_config_file_override():
+    sys.argv.append('test')
+    parser = get_argparser(prog='myapp', version='1.7',
+                           default_config='test/myapp.conf')
+    parser.add_argument('username')
+    args = parser.parse_args()
+    assert args.config_file == 'test/myapp.conf'
+    assert args.default_config_file == 'test/myapp.conf'
     assert args.verbose == 0
-    assert args.version == '1.7'
+    assert args.loglevel == 30
+    assert args.prog == 'myapp'
+    assert args.quiet == False
+    assert args.username == 'test'
+    sys.argv.pop()
 
 
 def test_no_default_config():
