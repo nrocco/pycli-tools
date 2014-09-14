19a73196f7415cfa64ac52e948225d359d6728a2
Nico Di Rocco <dirocco.nico@gmail.com>
2014-09-14 23:12:48 +0200

    Reorganized setuptools tests

--- setup.py
+++ setup.py
@@ -1,37 +1,51 @@
-#!/usr/bin/env python
-import os
+# -*- coding: utf-8 -*-
 import re
+import codecs
 
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 
-with open(os.path.join(os.path.dirname(__file__), 'pycli_tools',
-                       '__init__.py'), 'rb') as file:
-    content = file.read()
-    VERSION = re.compile(r".*__version__ = '(.*?)'", re.S).match(content).group(1)
-    AUTHOR = re.compile(r".*__author__ = '(.*?)'", re.S).match(content).group(1)
+class NoseTestCommand(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # Run nose ensuring that argv simulates running nosetests directly
+        import nose
+        nose.run_exit(argv=['nosetests'])
 
 
 setup(
     name = 'pycli_tools',
     description = 'A python module to help create predictable command line tools for python >= 2.6 and 3.x',
-    version = VERSION,
-
-    packages = find_packages(),
-    download_url = 'http://github.com/nrocco/pycli-tools',
-    url = 'http://nrocco.github.io/',
-
-    author = AUTHOR,
+    version = re.search(r'''^__version__\s*=\s*["'](.*)["']''', open('pycli_tools/__init__.py').read(), re.M).group(1),
+    author = 'Nico Di Rocco',
     author_email = 'dirocco.nico@gmail.com',
-
-    long_description = open('README.rst').read(),
+    url = 'http://nrocco.github.io/',
+    license = 'GPLv3',
+    download_url = 'http://github.com/nrocco/pycli-tools',
+    long_description = codecs.open('README.rst', 'rb', 'utf-8').read(),
     include_package_data = True,
-    license = open('LICENSE').read(),
     zip_safe = False,
+    tests_require = [
+        'nose',
+        'mock',
+        'coverage',
+    ],
+    packages = [
+        'pycli_tools'
+    ],
     classifiers = [
         'Operating System :: OS Independent',
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
@@ -41,4 +55,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    cmdclass = {
+        'test': NoseTestCommand
+    }
 )
